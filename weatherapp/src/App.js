9c098274e2274a9838123e2c25f5daaafbc8c9f0
JRodDvlpr <Mialienproductions@gmail.com>
2019-12-02 14:02:40 -0500

    Successfull Response from API

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,10 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import Axios from 'axios';
+import axios from 'axios';
 
 function App() {
 
+  const [weather, setWeather] = useState([]);
 
+  useEffect(() => {
+    axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=4159050&units=imperial&appid=0ff895f1354b494c95ab7dfadda2a141`)
+      .then(arr => {
+        console.log(arr);
+        setWeather(arr.data);
+      })
+      .catch(err => console.log(`There error is: ${err}`));
+    },[]);  
 
   return (
     <div className="App">
