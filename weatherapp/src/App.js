a0fdbd43d050f4026849acf642e100a0e6463322
JRodDvlpr <Mialienproductions@gmail.com>
2019-12-02 15:54:17 -0500

    components added

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -2,12 +2,14 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from 'axios';
 
+const API_Key = '0ff895f1354b494c95ab7dfadda2a141';
+
 function App() {
 
   const [weather, setWeather] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=4159050&units=imperial&appid=0ff895f1354b494c95ab7dfadda2a141`)
+    axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=4159050&units=imperial&appid=${API_Key}`)
       .then(arr => {
         console.log(arr);
         setWeather(arr.data);
