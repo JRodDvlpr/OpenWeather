52f242bdb9ecc3be9060ac4c8f008bcf27f818d8
JRodDvlpr <Mialienproductions@gmail.com>
2020-01-14 13:39:04 -0500

    Working Weather app

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,60 +1,118 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
-import HeaderCard from './components/Header';
+import Header from './components/Header';
 import WeatherCard from './components/WeatherCard';
+import Form from './components/Form';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-//const API_Key = '0ff895f1354b494c95ab7dfadda2a141';
+const API_Key = '0ff895f1354b494c95ab7dfadda2a141';
 
-function App() {
-  const [location, setLocation] = useState([]);
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      ferenheit: undefined,
+      description: "",
+      error: false
+    };
+ 
+   // weather icons depending on the weather
+    // this.weatherIcon = {
+    //   Thunderstorm: "wi-thunderstorm",
+    //   Drizzle: "wi-sleet",
+    //   Rain: "wi-storm-showers",
+    //   Snow: "wi-snow",
+    //   Atmosphere: "wi-fog",
+    //   Clear: "wi-day-sunny",
+    //   Clouds: "wi-day-fog"
+    // };
+  }
   
-  const [weather, setWeather] = useState({
-    temp: '',
-    city: '',
-    condition: '',
-    description: '',
-    country: ''
-  })
+  // check weather and specify that icon to the state
+  // get_WeatherIcon(icons, rangeId) {
+  //   switch (true) {
+  //     case rangeId >= 200 && rangeId < 232:
+  //       this.setState({ icon: icons.Thunderstorm });
+  //       break;
+  //     case rangeId >= 300 && rangeId <= 321:
+  //       this.setState({ icon: icons.Drizzle });
+  //       break;
+  //     case rangeId >= 500 && rangeId <= 521:
+  //       this.setState({ icon: icons.Rain });
+  //       break;
+  //     case rangeId >= 600 && rangeId <= 622:
+  //       this.setState({ icon: icons.Snow });
+  //       break;
+  //     case rangeId >= 701 && rangeId <= 781:
+  //       this.setState({ icon: icons.Atmosphere });
+  //       break;
+  //     case rangeId === 800:
+  //       this.setState({ icon: icons.Clear });
+  //       break;
+  //     case rangeId >= 801 && rangeId <= 804:
+  //       this.setState({ icon: icons.Clouds });
+  //       break;
+  //     default:
+  //       this.setState({ icon: icons.Clouds });
+  //   }
+  // }
 
-  
-  useEffect((e) => {
+  // calculate celsius
+  // calCelsius(temp) {
+  //   let cell = Math.floor(temp - 273.15);
+  //   return cell;
+  // }
 
-    // const city = e.target.city.value;
+  // event handler for submit button in form component
+  getWeather = async e => {
+    e.preventDefault();
 
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?${location},&appid=0ff895f1354b494c95ab7dfadda2a141`)
-      .then(res => {
-        console.log(res.data);
-        setWeather({
-          temp: res.main.temp,
-          city: res.name,
-          condition: res.weather[0].main,
-          country: res.sys.country
-        })
-      })
-      .catch(err => console.log(`There error is: ${err}`));
-    },[]);  
+    // get values from text box
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
 
-    const handleSearch = event => {
-      setWeather(event.target.elements.country.value);
-     }
-    
+    if(country && city) {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country},us&units=metric&appid=${API_Key}`);
 
-  return (
-    <section className="App">
-      <form>
-    <HeaderCard />
-    {/* <InputForm value={location} onChange={(e) => setWeather(e.target.value)} /> */}
-    <input name="city" onChange={(e) => setLocation(e.target.value)}
-        />
-    <button  onClick={event => handleSearch(event)} type='submit'>Get Weather</button>
-    <h2>City:</h2>
-    <WeatherCard temp={weather.temp} condition={weather.condition} city={weather.city} country={weather.country} />
-    </form>
+    const response = await api_call.json();
 
-    </section>
+    this.setState({
+      city: `${response.name}, ${response.sys.country}`,
+      country: response.sys.country,
+      main: response.weather[0].main,
+      temp: response.main.temp,
+      description: response.weather[0].description,
+      error: false
+    });
+
+    // // seting icons
+    // this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+ 
+    console.log(response);
+  } else {
+    this.setState({
+      error: true
+    });
+  }
+};
+
    
+render() {
+  return (
+    <div className="App">
+      <Form loadweather={this.getWeather} error={this.state.error} />
+      <WeatherCard
+        cityname={this.state.city}
+        weatherIcon={this.state.icon}
+        temp={this.state.temp}
+        description={this.state.description}
+      />
+    </div>
   );
+}
 }
 
 export default App;
--- weatherapp/src/components/WeatherCard.js
+++ weatherapp/src/components/WeatherCard.js
@@ -1,15 +1,25 @@
-// Weather information displayed 
-import React from 'react';
-import WeatherDetails from './WeatherDetails';
-
-
-const CardWeather = () => {
-
-    return (
-        <section className="cardWeather">
-            <WeatherDetails />
-        </section>
-    )
-}
-
-export default CardWeather;
+import React from "react";
+import '../components/weather.css';
+ 
+const Weather = props => {
+  return (
+    <div className="container text-dark">
+      <div className="Card">
+        <h1 className="text-black py-3">{props.cityname}</h1>
+        
+        {/* Get Celsius */}
+        {props.temp ? (
+          <h1 className="py-2">{props.temp}&deg;</h1>
+        ) : null}
+ 
+        {/* Weather description */}
+        <h4 className="py-3">
+          {props.description.charAt(0).toUpperCase() +
+            props.description.slice(1)}
+        </h4>
+      </div>
+    </div>
+  );
+};
+ 
+export default Weather;
